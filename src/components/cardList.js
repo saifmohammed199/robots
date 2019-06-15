95c0fe49f791880addfd80630db23e0dadf100ed
Saif Mohammed <saifmohammedbloger@gmail.com>
2019-06-15 17:11:01 +0530

    project completed and uploaded

---
+++ src/components/cardList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './card';
+
+
+const CardList = ({robots}) =>{
+    return(
+        <div>
+            {
+                robots.map((user,i)=>{
+                    return(
+                        <Card
+                        key={i}
+                        id={robots[i].name}
+                        name={robots[i].name}
+                        email={robots[i].email}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default CardList;
